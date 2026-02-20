604937aff6f27eb0add2d9a953ea234dbe9f9f93
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2026-02-20 16:43:22 +0300

    refactor: AppNavigation constructor argument change + startDestination type change from Any to Route

--- composeApp/src/commonMain/kotlin/ru/kazan/itis/bikmukhametov/kts/presentation/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/ru/kazan/itis/bikmukhametov/kts/presentation/navigation/AppNavigation.kt
@@ -4,15 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import ru.kazan.itis.bikmukhametov.kts.presentation.screens.LoginScreen
 import ru.kazan.itis.bikmukhametov.kts.presentation.screens.OnboardingScreen
 
 /* Навигация приложения */
 @Composable
 fun AppNavigation(
-    navController: NavHostController = rememberNavController(),
-    startDestination: Any = Route.Onboarding
+    navController: NavHostController,
+    startDestination: Route = Route.Onboarding
 ) {
     NavHost(
         navController = navController,
